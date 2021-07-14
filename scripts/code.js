a17fe178ebefa3a4e7ab2915c28b384efbd28e04
USTower7 <78798982+USTower7@users.noreply.github.com>
2021-07-14 21:51:22 +0300

    My workd add filter box

--- scripts/code.js
+++ scripts/code.js
@@ -7,18 +7,37 @@ function backToTop() {
     }
 }
 
-
-
 window.addEventListener('scroll', function () {
     arrowTop.hidden = (pageYOffset < document.documentElement.clientHeight);
 });
 
-
-
-
-
-menu.onclick = function() {
+menu.onclick = function () {
     menu.classList.toggle('active');
     menuList.classList.toggle('active');
     body.classList.toggle('active');
 }
+
+const filterBox = document.querySelectorAll('.works__item');
+
+document.querySelector('.nav-filter').addEventListener('click', elem => {
+    if (elem.target.tagName !== 'LI') return false
+
+    const filterClass = elem.target.dataset['filter'];
+
+    filterBox.forEach(elem => {
+        if (!elem.classList.contains(filterClass) && filterClass !== 'all') {
+            elem.classList.add('anime')
+        } else {
+            elem.classList.remove('anime')
+            elem.classList.remove('hide')
+        }
+    })
+
+    filterBox.forEach(elem => {
+        elem.ontransitionend = () => {
+            if (elem.classList.contains('anime')) {
+                elem.classList.add('hide')
+            }
+        }
+    })
+})
